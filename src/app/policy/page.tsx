8897b89410f503954f72b7a63dc80d1d454a2e94
DysDan <dtobarl@unal.edu.co>
2025-01-10 17:18:29 -0500

    added policy page (wip)
    es re larga lmao

---
+++ src/app/policy/page.tsx
@@ -0,0 +1,205 @@
+"use client";
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import { Header } from "../../../components";
+
+export default function Home() {
+  const { data: session } = useSession();
+  const [message, setMessage] = useState<string | null>(null);
+
+  useEffect(() => {
+    // Leer el mensaje desde localStorage
+    const flashMessage = localStorage.getItem("flashMessage");
+    
+    if (flashMessage) {
+      setMessage(flashMessage);
+      // Eliminar el mensaje después de mostrarlo
+      localStorage.removeItem("flashMessage");
+    }
+  }, []);
+
+  return (
+    <div>
+      <Header />
+      <div className="flex min-h-screen flex-col items-center justify-center p-4 bg-gray-100">
+        <h1 className="text-2xl font-bold text-gray-800 mb-4 text-center">
+            POLÍTICA DE TRATAMIENTO DE DATOS PERSONALES
+        </h1>
+
+        {/* 1 */}
+        <div className="bg-white p-8 rounded-lg shadow-lg max-w-2xl mb-6">
+          <p className="text-justify text-gray-700">
+            La Ley 1581 de 2012, reglamentada parcialmente por el Decreto 1377 de 2013, tiene por objeto desarrollar el derecho constitucional que tienen todas las personas a conocer, actualizar y rectificar 
+            las informaciones que se hayan recogido sobre ellas en bases de datos o archivos, y, los demás derechos, libertades y garantías constitucionales de que trata el artículo 15 de la Constitución Política, 
+            así como el derecho a la información consagrado en el artículo 20 de la misma Carta.<br></br>
+            Dando cumplimiento a lo establecido por la Ley 1581 de 2012, por la cual se dictan disposiciones para la protección de datos personales, Guayaba, en su calidad de responsable del tratamiento de datos personales
+            , formula las políticas generales que deben seguirse:
+          </p>
+        </div>
+
+        {/* 1.1 */}
+        <div className="bg-white p-8 rounded-lg shadow-lg max-w-2xl mb-6">
+          <h1 className="text-2xl font-bold text-gray-800 mb-4 text-center">
+            1.1 Identificación del Responsable del Tratamiento de Datos Personales
+          </h1>
+          <p className="text-justify text-gray-700">
+            El Responsable del Tratamiento de Datos Personales es Santiago Reyes Ochoa, identificado con C.C 1025460191 con correo electrónico: sreyeso@unal.edu.co.
+            <br></br>
+            <br></br>
+            Contacto: A través de cualquiera de los siguientes canales de Atención al cliente que Santiago Reyes Ochoa tiene dispuestos:
+            <br></br>
+            • Escrito: Calle 75 # 92-30 en Bogotá. Correo Electrónico: guayabagestion@gmail.com<br></br>
+            • Celular: +57 314 8073225<br></br>
+            <br></br>
+            Es encargado del tratamiento de los datos personales Santiago Reyes Ochoa, según el tipo de información que maneja en el cumplimiento de sus funciones.
+          </p>
+        </div>
+
+        {/* 2 */}
+        <div className="bg-white p-8 rounded-lg shadow-lg max-w-2xl mb-6">
+          <h1 className="text-2xl font-bold text-gray-800 mb-4 text-center">
+            2. Objetivo
+          </h1>
+          <p className="text-justify text-gray-700">
+          Establecer la política para el tratamiento de datos de Guayaba para la debida protección de los derechos de los usuarios que suministran sus datos personales a la entidad por las diferentes vías de atención y los diferentes medios de recolección de información.<br></br>
+          La política de tratamiento de datos que presenta este documento hace parte integral de la política de seguridad y privacidad de la información.
+          </p>
+        </div>
+
+
+        {/* 3 */}
+        <div className="bg-white p-8 rounded-lg shadow-lg max-w-2xl mb-6">
+          <h1 className="text-2xl font-bold text-gray-800 mb-4 text-center">
+            3. Alcance
+          </h1>
+          <p className="text-justify text-gray-700">
+            Esta política de protección de datos aplica a todos los servidores públicos colaboradores, proveedores y personal externo que tenga acceso a los datos personales recogidos por Guayaba. Igualmente aplica a toda la información determinada como dato personal que se encuentre registrada en bases de datos, archivos de información o cualquier medio de almacenamiento en poder de Guayaba y que por sus características particulares haya sido contemplada en la Ley 1581 de 2012.
+          </p>
+        </div>
+
+        {/* 4 */}
+        <div className="bg-white p-8 rounded-lg shadow-lg max-w-2xl mb-6">
+          <h1 className="text-2xl font-bold text-gray-800 mb-4 text-center">
+            4. Obligaciones
+          </h1>
+          <p className="text-justify text-gray-700">
+            Todos los funcionarios, contratistas, personal que labore con Guayaba, vinculado con proveedores, o personal externo de la Entidad, que por sus labores recolecten, almacenen, usen, circulen o supriman datos cuyo encargado de tratamiento sea Santiago Reyes Ochoa, deberán conocer y dar cumplimiento a la presente política.
+          </p>
+        </div>
+
+        {/* 5 */}
+        <div className="bg-white p-8 rounded-lg shadow-lg max-w-2xl mb-6">
+          <h1 className="text-2xl font-bold text-gray-800 mb-4 text-center">
+            5. Definiciones
+          </h1>
+          <p className="text-justify text-gray-700">
+          Para efectos de la aplicación de la presente política, se tendrán en cuenta las definiciones establecidas en el artículo 3 de la Ley 1581 de 2012, el artículo 3 del Decreto 1377 de 2013, en el artículo 3 de la Ley 1266 de 2008, o el que haga sus veces:
+          <br></br><br></br>
+            a. <strong>Autorización:</strong> Consentimiento previo, expreso e informado del Titular para llevar a cabo el Tratamiento de datos personales.
+          <br></br><br></br>
+            b. <strong>Aviso de Privacidad:</strong> Comunicación verbal o escrita generada por el responsable, dirigida al titular para el tratamiento de sus datos personales, mediante la cual se le informa acerca de la existencia de las políticas de tratamiento de información que le serán aplicables, la forma de acceder a las mismas y las finalidades del tratamiento que se pretende dar a los datos personales.
+          <br></br><br></br>
+            c. <strong>Base de Datos:</strong> Conjunto organizado de datos personales que sea objeto de Tratamiento.
+          <br></br><br></br>
+            d. <strong>Dato Personal:</strong> Cualquier información vinculada o que pueda asociarse a una o varias personas naturales determinadas o determinables.
+          <br></br><br></br>
+            e. <strong>Dato Público:</strong> Es el dato calificado como tal según los mandatos de la Ley o de la Constitución Política y todos aquellos que no sean semiprivados o privados, de conformidad con la Ley 1266 de 2008. Son públicos, entre otros, los datos contenidos en documentos públicos, sentencias judiciales debidamente ejecutoriadas que no estén sometidos a reserva y los relativos al estado civil de las personas.
+          <br></br><br></br>
+            La ley 1581 de 2012 establece las siguientes categorías especiales de datos personales:
+          <br></br><br></br>
+            f. <strong>Datos sensibles:</strong> Son aquellos que afectan la intimidad del Titular o cuyo uso indebido puede generar su discriminación, tales como aquellos que revelen el origen racial o étnico, la orientación política, las convicciones religiosas o filosóficas, la pertenencia a sindicatos, organizaciones sociales, de derechos humanos o que promueva intereses de cualquier partido político o que garanticen los derechos y garantías de partidos políticos de oposición, así como los datos relativos a la salud, a la vida sexual y los datos biométricos.
+          <br></br><br></br>
+            g. <strong>Datos personales de los niños, niñas y adolescentes:</strong> Se debe tener en cuenta que aunque la Ley 1581 de 2012 prohíbe el tratamiento de los datos personales de los niños, niñas y adolescentes, salvo aquellos que por su naturaleza son públicos, la Corte Constitucional precisó en la Sentencia C-748/11 que independientemente de la naturaleza del dato, se puede realizar el tratamiento de éstos “siempre y cuando el fin que se persiga con dicho tratamiento responda al interés superior de los niños, niñas y adolescentes y se asegure sin excepción alguna el respeto a sus derechos prevalentes”.
+          <br></br><br></br>
+            h. <strong>Encargado del Tratamiento:</strong> Persona natural o jurídica, pública o privada, que por sí misma o en asocio con otros, realice el Tratamiento de datos personales por cuenta del Responsable del Tratamiento.
+          <br></br><br></br>
+            i. <strong>Responsable de Tratamiento:</strong> Persona natural o jurídica, pública o privada, que por sí misma o en asocio con otros, decida sobre la base de datos y/o el Tratamiento de los datos.
+          <br></br><br></br>
+            j. <strong>Titular:</strong> Persona natural cuyos datos personales sean objeto de Tratamiento.
+          <br></br><br></br>
+            k. <strong>Tratamiento:</strong> Cualquier operación o conjunto de operaciones sobre datos personales, tales como la recolección, almacenamiento, uso, circulación o supresión.
+          <br></br><br></br>
+            l. <strong>Transferencia:</strong> La transferencia de datos tiene lugar cuando el responsable y/o encargado del tratamiento de datos personales, ubicado en Colombia, envía la información o los datos personales a un receptor, que a su vez es responsable del tratamiento y se encuentra dentro o fuera del país.
+          <br></br><br></br>
+            m. <strong>Transmisión:</strong> Tratamiento de datos personales que implica la comunicación de los mismos dentro o fuera del territorio de la Republica de Colombia cuando tenga por objeto la realización de un tratamiento por el encargado por cuenta del responsable.
+          </p>
+        </div>
+
+        {/* 6 */}
+        <div className="bg-white p-8 rounded-lg shadow-lg max-w-2xl mb-6">
+          <h1 className="text-2xl font-bold text-gray-800 mb-4 text-center">
+            6. Principios generales para tratamiento de datos personales
+          </h1>
+          <p className="text-justify text-gray-700">
+          En aplicación de la Ley 1581 de 2012 y las normas que la complementan, modifican o adicionan, Guayaba, aplicará de manera armónica e integral los principios establecidos en el artículo 4º de la Ley 1581 de 2012. <br></br><br></br>
+            <strong>Principio de legalidad:</strong> La recolección, uso y tratamiento de datos personales se fundamentará en lo establecido por la Ley y las demás disposiciones que la desarrollen.
+          <br></br><br></br>
+            <strong>Principio de finalidad:</strong> La recolección, uso y tratamiento de datos personales obedecerán a una finalidad legítima de acuerdo con la Constitución y la Ley, la cual será informada al titular de los datos.
+          <br></br><br></br>
+            <strong>Principio de libertad:</strong> La recolección, uso y tratamiento de datos personales sólo puede ejercerse con el consentimiento previo, expreso e informado del titular. Los datos personales no podrán ser obtenidos o divulgados sin previa autorización, o en ausencia de mandato legal o judicial que releve el consentimiento.
+          <br></br><br></br>
+            <strong>Principio de veracidad o calidad:</strong> La información sujeta a tratamiento debe ser veraz, completa, exacta, actualizada, comprobable y comprensible. Se prohíbe el tratamiento de datos parciales, incompletos, fraccionados o que induzcan a error.
+          <br></br><br></br>
+            <strong>Principio de transparencia:</strong> En la recolección, uso y tratamiento de datos personales debe garantizarse el derecho del titular a obtener del responsable del tratamiento o del encargado, en cualquier momento y sin restricciones, información acerca de la existencia de datos que le conciernan.
+          <br></br><br></br>
+            <strong>Principio de acceso y circulación restringida:</strong> La recolección, uso y tratamiento de datos sólo podrá hacerse por personas autorizadas por el titular y/o por las personas previstas en la Ley y demás normas que la desarrollan.
+          <br></br><br></br>
+            Los datos personales, salvo la información pública, no podrán estar disponibles en Internet u otros medios de divulgación o comunicación masiva, salvo que el acceso sea técnicamente controlable para brindar un conocimiento restringido sólo a los titulares o terceros autorizados conforme a la ley.
+          <br></br><br></br>
+            <strong>Principio de seguridad:</strong> Los datos personales e información sujeta a tratamiento público, será objeto de protección y deberá manejarse con las medidas y recursos técnicos, humanos y administrativos que sean necesarios para brindar seguridad a los registros, así como con la adopción de herramientas tecnológicas de protección, evitando su adulteración, pérdida, consulta, uso o acceso no autorizado o fraudulento.
+          <br></br><br></br>
+            <strong>Principio de confidencialidad:</strong> Todas las personas que intervengan en la recolección, uso y tratamiento de datos personales que no tengan la naturaleza de públicos están obligadas a garantizar la reserva de la información, incluso luego de finalizada su relación con alguna de las labores que comprende el tratamiento.
+          </p>
+        </div>
+
+        {/* 7 */}
+        <div className="bg-white p-8 rounded-lg shadow-lg max-w-2xl mb-6">
+          <h1 className="text-2xl font-bold text-gray-800 mb-4 text-center">
+            7. Tratamiento de datos personales
+          </h1>
+          <p className="text-justify text-gray-700">
+          El usuario, visitante, ciudadano y toda persona que suministra sus datos personales a Guayaba, por cualquier medio, lo hace voluntariamente y por la necesidad de obtener un servicio de la entidad, o presentar una queja o reclamo, o acceder a los sitios o aplicativos web. Lo anterior significa que Guayaba podrá capturar sus datos personales.
+          <br></br><br></br>
+          En este sentido, los datos personales que recolecta, almacena, usa o suprime Guayaba en sus bases de datos, se utilizarán única y exclusivamente para el cumplimiento de sus funciones y no serán cedidos a terceros para fines distintos, sin la debida autorización del titular de la información.
+          <br></br><br></br>
+          En todo caso, para el almacenamiento y uso de la información personal, Guayaba se regirá por lo dispuesto en la Ley 1581 de 2012 y el Decreto 1377 de 2013, o las que hagan sus veces, teniendo en cuenta que toda persona tiene derecho a conocer, rectificar y actualizar la información que la Entidad registre en las bases de datos o archivos susceptibles de tratamiento.
+          <br></br><br></br>
+          En general, el tratamiento de los datos sensibles que sean recolectados por Guayaba, se hará de conformidad con lo establecido en el título III de la Ley 1581 de 2012 y las demás normas que la desarrollen o complemente.
+          </p>
+        </div>
+
+        {/* 8 */}
+        <div className="bg-white p-8 rounded-lg shadow-lg max-w-2xl mb-6">
+          <h1 className="text-2xl font-bold text-gray-800 mb-4 text-center">
+            8. Personas a quienes Guayaba puede suministrar la información
+          </h1>
+          <p className="text-justify text-gray-700">
+            La información y datos personales que reúnan las condiciones establecidas por la ley y las demás normas que la desarrollan, siguiendo lo establecido en esta política, podrán suministrarse a las siguientes personas:
+            <br></br><br></br>
+            •	A los titulares o sus representantes legales.
+            <br></br><br></br>
+            •	A las entidades públicas o administrativas en ejercicio de sus funciones legales o por orden judicial.
+            <br></br><br></br>
+            •	A los terceros autorizados por el titular o por la ley.
+          </p>
+        </div>
+
+        {/* 9 */}
+        <div className="bg-white p-8 rounded-lg shadow-lg max-w-2xl mb-6">
+          <h1 className="text-2xl font-bold text-gray-800 mb-4 text-center">
+            9. Tratamiento y finalidad
+          </h1>
+          <p className="text-justify text-gray-700">
+            El tratamiento que realizará Santiago Reyes Ochoa como responsable del tratamiento de datos personales con la información personal será el siguiente: 
+            <br></br><br></br>
+            •	Recopilar información de asistentes a eventos, cursos, capacitaciones y cualquier otra actividad desarrollada por la Agencia o en las que ésta participe.
+            <br></br><br></br>
+            •	Atender y evaluar la satisfacción de los usuarios frente a las peticiones, quejas, reclamos y denuncias presentadas.
+            <br></br><br></br>
+            La recolección, almacenamiento, uso, circulación, supresión, actualización y gestión de los datos personales capturados por Guayaba, se realizará en ejercicio de sus funciones legales. 
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
